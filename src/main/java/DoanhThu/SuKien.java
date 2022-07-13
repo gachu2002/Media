16b5213cfa08ce20dfcff11fe40365a0dea69f8a
trongducKT <buitrongducktmr@gmail.com>
2022-07-13 17:13:38 +0700

    SuKien

--- src/main/java/DoanhThu/SuKien.java
+++ src/main/java/DoanhThu/SuKien.java
@@ -29,59 +29,72 @@ TODO4: chia rõ các class Quản lý với các class View (?)
 TODO5: NhanVien: ArrayList<NhanVien>, them, xoa, ...
  */
 public class SuKien {
-
-    public SuKien() {
-    }
-
-    private SanPham sp;
-    private Date ngay;
-    private String hanhdong;
-    private int soluong_hd;
-
-    public int getSoluong_hd() {
-        return soluong_hd;
-    }
-
-    public void setSoluong_hd(int soluong_hd) {
-        this.soluong_hd = soluong_hd;
-    }
-
-    public SanPham getSp() {
-        return sp;
-    }
-
-    public void setSp(SanPham sp) {
-        this.sp = sp;
-    }
-
-    public Date getNgay() {
-        return ngay;
-    }
-
-    public void setNgay(Date ngay) {
-        this.ngay = ngay;
-    }
-
-    public String getHanhdong() {
-        return hanhdong;
-    }
-
-    public void setHanhdong(String hanhdong) {
-        this.hanhdong = hanhdong;
-    }
-
-    public SuKien(SanPham sp, Date ngay, String hanhdong, int soluong_hd) {
-        this.sp = sp;
-        this.ngay = ngay;
-        this.hanhdong = hanhdong;
-        this.soluong_hd = soluong_hd;
-    }
-
-    public void isRealSK() {
-        if (this.hanhdong.equals("mua")) {
-            this.sp.setSoLuong(this.sp.getSoluong() + this.soluong_hd);
-        } else if (this.hanhdong.equals("ban") & this.sp.getSoluong() > this.soluong_hd - 1) {
-            this.sp.setSoLuong(this.sp.getSoluong() - this.soluong_hd);
-        }
-    }
+     private String tenSuKien;
+     private String loaiSuKien;
+     public String getTenSuKien(){
+           return this.tenSuKien;
+     }
+     public String getLoaiSuKien(){
+           return this.loaiSuKien;
+     }
+     public void setTenSuKien(String tenSuKien){
+           this.tenSuKien=tenSuKien;
+     }
+     public void setLoaiSuKien(String loaiSuKien){
+           this.loaiSuKien=loaiSuKien;
+     }
+//    public SuKien() {
+//    }
+//
+//    private SanPham sp;
+//    private Date ngay;
+//    private String hanhdong;
+//    private int soluong_hd;
+//
+//    public int getSoluong_hd() {
+//        return soluong_hd;
+//    }
+//
+//    public void setSoluong_hd(int soluong_hd) {
+//        this.soluong_hd = soluong_hd;
+//    }
+//
+//    public SanPham getSp() {
+//        return sp;
+//    }
+//
+//    public void setSp(SanPham sp) {
+//        this.sp = sp;
+//    }
+//
+//    public Date getNgay() {
+//        return ngay;
+//    }
+//
+//    public void setNgay(Date ngay) {
+//        this.ngay = ngay;
+//    }
+//
+//    public String getHanhdong() {
+//        return hanhdong;
+//    }
+//
+//    public void setHanhdong(String hanhdong) {
+//        this.hanhdong = hanhdong;
+//    }
+//
+//    public SuKien(SanPham sp, Date ngay, String hanhdong, int soluong_hd) {
+//        this.sp = sp;
+//        this.ngay = ngay;
+//        this.hanhdong = hanhdong;
+//        this.soluong_hd = soluong_hd;
+//    }
+//
+//    public void isRealSK() {
+//        if (this.hanhdong.equals("mua")) {
+//            this.sp.setSoLuong(this.sp.getSoluong() + this.soluong_hd);
+//        } else if (this.hanhdong.equals("ban") & this.sp.getSoluong() > this.soluong_hd - 1) {
+//            this.sp.setSoLuong(this.sp.getSoluong() - this.soluong_hd);
+//        }
+//    }
 }
--- src/main/java/DoanhThu/SuKienMotLan.java
+++ src/main/java/DoanhThu/SuKienMotLan.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DoanhThu;
 
-/**
- *
- * @author XPS
- */
+import java.time.LocalDateTime;
 public class SuKienMotLan extends SuKien {
-    
+    private LocalDateTime date;
+    private int giatri;
+    public int getGiatri() {
+        return giatri;
+    }
+    public LocalDateTime getDate(){
+        return date;
+    }
 }
