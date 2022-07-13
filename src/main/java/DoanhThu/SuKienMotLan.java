41addc5aa6fea740028b316a06c6e8063fa7f31f
trongducKT <buitrongducktmr@gmail.com>
2022-07-13 18:32:35 +0700

    finsh SK

--- src/main/java/DoanhThu/SuKienMotLan.java
+++ src/main/java/DoanhThu/SuKienMotLan.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 public class SuKienMotLan extends SuKien {
     private LocalDateTime date;
     private int giatri;
+
+    public SuKienMotLan(String string, String string0, String string1, int parseInt) {
+        super();
+        
+    }
     public int getGiatri() {
         return giatri;
     }
