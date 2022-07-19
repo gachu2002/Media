20e7a6bc275461fc6c04884ce03851d587bd5d02
Tung <phamduytung2002@gmail.com>
2022-07-19 20:10:14 +0700

    add .class files

---
+++ src/main/java/NhanVien/NVPartTime.java
@@ -0,0 +1,38 @@
+package NhanVien;
+
+public class NVPartTime extends NhanVien {
+
+    private double thoiGianLamViec;
+    private static double luongTheoGio = 18000;
+
+    public NVPartTime() {
+    }
+
+    public void setLuongTheoGio(double s) {
+        luongTheoGio = s;
+    }
+
+    public double getThoiGianLamViec() {
+        return thoiGianLamViec;
+    }
+
+    public double getLuongTheoGio() {
+        return this.luongTheoGio;
+    }
+
+    public void setThoiGianLamViec(double s) {
+        this.thoiGianLamViec = s;
+    }
+
+    @Override
+    public double tinhLuong() {
+        return this.thoiGianLamViec * luongTheoGio;
+    }
+    public String getLoaiNhanVien(){
+       return "Part-time";
+    }
+    public NVPartTime(String name, double thoiGianLamViec) {
+        setTenNhanVien(name);
+        this.thoiGianLamViec = thoiGianLamViec;
+    }
+}
